dd6e9fbd94bb7a75751082efb1653a127a980f32
Brian Moseley <bcm@maz.org>
2014-01-26 10:50:03 -0800

    add jquery jsend plugin

---
+++ vendor/assets/javascripts/jquery.jsend.js
@@ -0,0 +1,22 @@
+jQuery(function() {
+  jQuery.getJSend = function(url, data_success) {
+    var dfd = jQuery.Deferred()
+    jQuery.getJSON(url, data, success).
+      done(function(data, status, xhr) {
+        if (data.status == 'success') {
+          dfd.resolve(data.data)
+        } else if (data.status == 'fail') {
+          dfd.reject(data.data)
+        } else if (data.status == 'error') {
+          console.log("JSend error [" + (data.code || '-') + "]: " + data.message + "(" + JSON.stringify(data.data) +
+                      ")")
+        } else {
+          console.log("Unknown JSend status " + data.status)
+        }
+      }).
+      fail(function(xhr, status, error) {
+        console.log("Ajax error [" + status + "]: " + error)
+      });
+    return dfd.promise();
+  }
+});
