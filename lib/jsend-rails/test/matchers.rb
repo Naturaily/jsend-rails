4e028e56c18ac1a8aba8aef9af7588df4a6df193
Brian Moseley <bcm@maz.org>
2014-09-05 12:38:01 -0700

    rspec 3 compatibility

--- lib/jsend-rails/test/matchers.rb
+++ lib/jsend-rails/test/matchers.rb
@@ -9,48 +9,52 @@ module JSend
           match do |response|
             response.content_type =~ /^application\/json/
           end
-          failure_message_for_should do |response|
+          failure_message do |response|
             "Response should have application/json content type"
           end
-          failure_message_for_should_not do |response|
+          failure_message_when_negated do |response|
             "Response should not have application/json content type"
           end
+          alias negative_failure_message failure_message_when_negated
         end
 
         RSpec::Matchers.define :be_jsend_success do
           match do |response|
             response.json['status'].should == 'success'
           end
-          failure_message_for_should do |response|
+          failure_message do |response|
             "Response should be JSend success"
           end
-          failure_message_for_should_not do |response|
+          failure_message_when_negated do |response|
             "Response should not be JSend success"
           end
+          alias negative_failure_message failure_message_when_negated
         end
 
         RSpec::Matchers.define :be_jsend_failure do
           match do |response|
             response.json['status'].should == 'fail'
           end
-          failure_message_for_should do |response|
+          failure_message do |response|
             "Response should be JSend failure"
           end
-          failure_message_for_should_not do |response|
+          failure_message_when_negated do |response|
             "Response should not be JSend failure"
           end
+          alias negative_failure_message failure_message_when_negated
         end
 
         RSpec::Matchers.define :be_jsend_error do
           match do |response|
             response.json['status'].should == 'error'
           end
-          failure_message_for_should do |response|
+          failure_message do |response|
             "Response should be JSend error"
           end
-          failure_message_for_should_not do |response|
+          failure_message_when_negated do |response|
             "Response should not be JSend error"
           end
+          alias negative_failure_message failure_message_when_negated
         end
       end
     end
